7ab3f6da693d981d295d9ad60992c5bd5e0de6b9
Miguel Ledesma Palacios <mledpal@gmail.com>
2024-02-09 11:40:01 +0100

    Pusher habilitado

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -28,17 +28,17 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from "laravel-echo";
-// import Pusher from "pusher-js";
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 
-// window.Pusher = Pusher;
+window.Pusher = Pusher;
 
-// window.Echo = new Echo({
-//     broadcaster: "pusher",
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-//     forceTLS: true,
-// });
+window.Echo = new Echo({
+    broadcaster: "pusher",
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+});
 
 // import Echo from "@ably/laravel-echo";
 // import * as Ably from "ably";
