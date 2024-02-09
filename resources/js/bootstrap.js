959dbfe556d93387f87ea3057b25dd33945c36c3
Miguel Ledesma Palacios <mledpal@gmail.com>
2024-02-09 11:40:48 +0100

    Pusher habilitado 2

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,36 +9,21 @@ window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-// import Pusher from "pusher-js";
-
-// Pusher.logToConsole = true;
-
-// var pusher = new Pusher("5285b606cdf2c249808a", {
-//     cluster: "eu",
-// });
-
-// var channel = pusher.subscribe("canal-comentarios");
-// channel.bind("NuevoComentario", function (data) {
-//     console.log(JSON.stringify(data));
-// });
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-import Echo from "laravel-echo";
 import Pusher from "pusher-js";
 
-window.Pusher = Pusher;
+Pusher.logToConsole = true;
 
-window.Echo = new Echo({
-    broadcaster: "pusher",
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true,
+var pusher = new Pusher("5285b606cdf2c249808a", {
+    cluster: "eu",
 });
+
+var channel = pusher.subscribe("canal-comentarios");
+channel.bind("NuevoComentario", function (data) {
+    console.log(JSON.stringify(data));
+});
+
+
+// Ably
 
 // import Echo from "@ably/laravel-echo";
 // import * as Ably from "ably";
