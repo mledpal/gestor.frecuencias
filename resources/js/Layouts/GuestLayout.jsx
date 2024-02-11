59613fac6f7b6542d6bb0de03cc1bda642b2a2db
Miguel Ledesma Palacios <mledpal@gmail.com>
2024-02-11 10:41:39 +0100

    Elimino el logo de la pantalla de login para ampliar el espacio

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -5,10 +5,10 @@ export default function Guest({ children }) {
         <div className="max-h-screen max-w-screen flex flex-col sm:justify-center items-center sm:pt-0 bg-colorbg">
             <div>
                 <Link href="/" className="max-[1280px]:hidden block">
-                    <img
+                    {/* <img
                         src="/img/logo.webp"
                         className="w-[150px] h-[150px] text-gray-500 mb-10 max-[1280px]:mt-10 max-[1280px]:w-[75px] max-[1280px]:h-[75px]"
-                    />
+                    /> */}
                     {/* <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" /> */}
                 </Link>
             </div>
