75dbca2366ad5238fa09d46b6c2afa0f5a2cf87e
Miguel Ledesma Palacios <mledpal@gmail.com>
2024-02-15 19:21:18 +0100

    Inclusión de etiquetas Open Graph

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,14 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    {{-- OpenGraph --}}
+    <meta property=”og:title” content=”Gestión de Frecuencias 2.0” />
+    <meta property=”og:description” content=”App web para tener almacenados tus contactos de radio y poder compartirlas
+        con más radioaficionados.” />
+    <meta property=”og:url” content=”https://www.radioescucha.es” />
+    <meta property=”og:image” content=”/images/logo.webp” />
+    <meta property="og:site_name" content="Gestión de Frecuencias v2.0" />
+
     <!-- Scripts -->
     @routes
     @viteReactRefresh
