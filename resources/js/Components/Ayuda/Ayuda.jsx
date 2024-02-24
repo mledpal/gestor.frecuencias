e0becb52de638bd0774a1603b24050bddd032c20
Miguel Ledesma Palacios <mledpal@gmail.com>
2024-02-24 13:42:16 +0100

    Adaptación pantalla ayuda a moviles

--- resources/js/Components/Ayuda/Ayuda.jsx
+++ resources/js/Components/Ayuda/Ayuda.jsx
@@ -1,9 +1,21 @@
+import { useContext } from "react";
 import { BotonesFormulario } from "../BotonesFormulario/BotonesFormulario";
+import { AppContext } from "../AppProvider";
 
 export const Ayuda = ({ handleOpenHelp }) => {
+
+    const { isSmallScreen } = useContext(AppContext);
+
+    const clasesMovil = "w-screen h-screen ";
+    const clasesPC = "w-2/3 min-h-full ";
+    const clasesComunes = "bg-slate-800 shadow-[inset_-2px_2px_10px_rgba(255,255,255,.5),-2px_-2px_10px_rgba(0,0,0,.5)] flex flex-col items-center justify-between  rounded-xl m-auto select-none"
+
+    const clasesAyuda = isSmallScreen ? clasesMovil : clasesPC;
+
+
     return (
         <div
-            className="w-2/3 min-h-full bg-slate-800 shadow-[inset_-2px_2px_10px_rgba(255,255,255,.5),-2px_-2px_10px_rgba(0,0,0,.5)] flex flex-col items-center justify-between  rounded-xl m-auto select-none"
+            className={ clasesAyuda + clasesComunes }
             onClick={() => handleOpenHelp(null)}
         >
             <header className="h-15 w-full flex items-center justify-center bg-gradient-to-tl from-blue-900 bg-slate-800 rounded-tr-xl rounded-tl-xl p-5 font-bold text-xl shadow-[inset_2px_0_5px_rgba(255,255,255,.5),inset_-2px_0_5px_rgba(0,0,0,.5)]">
