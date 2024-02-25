73f2f0bfffb0a62fdf98c602d0da476953a08592
Miguel Ledesma Palacios <mledpal@gmail.com>
2024-02-25 19:07:54 +0100

    separo la lógica de Mensajes.jsx en un hook useMensajes

--- resources/js/Components/Mensajes/Mensajes.jsx
+++ resources/js/Components/Mensajes/Mensajes.jsx
@@ -1,81 +1,23 @@
-import { useEffect, useState } from "react";
-import { getConversaciones } from "@/Helpers/getConversaciones";
-import { borrarConversacion } from "./helpers/borrarConversacion";
-
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
 import { DatosUsuario } from "../Usuarios/Forms/DatosUsuario";
+import { useMensajes } from "./hooks/useMensajes";
 
 export const Mensajes = ({
     handleOpenUserSearcher,
     handleOpenSendMessage,
     setUserID,
-    userDB,
     setVista,
     isSmallScreen,
-    setID,
 }) => {
-    const [mensajes, setMensajes] = useState([]);
-    const clasesBotonesFormulario =
-        "cursor-pointer hover:scale-150 duration-150 hover:ease-in ease-linear select-none";
-
-    async function getData() {
-        let texto = await getConversaciones();
-        setMensajes(texto);
-    }
-
-    async function handleDeleteConversation(e, id) {
-        e.preventDefault();
-
-        withReactContent(Swal)
-            .fire({
-                title: "¿Desea eliminar la conversación?",
-                showDenyButton: true,
-                showCancelButton: false,
-                confirmButtonText: "Si",
-                denyButtonText: `No`,
-                icon: "question",
-            })
-            .then((result) => {
-                /* Read more about isConfirmed, isDenied below */
-                if (result.isConfirmed) {
-                    borrarConversacion(id);
-                    getData();
-                    const Toast = Swal.mixin({
-                        toast: true,
-                        position: "top-end",
-                        showConfirmButton: false,
-                        timer: 1000,
-                        timerProgressBar: true,
-                        didOpen: (toast) => {
-                            toast.onmouseenter = Swal.stopTimer;
-                            toast.onmouseleave = Swal.resumeTimer;
-                        },
-                    });
-                    Toast.fire({
-                        icon: "success",
-                        title: "Conversación borrada",
-                    });
-                } else if (result.isDenied) {
-                    Swal.fire("Cancelado", "", "info");
-                }
-            });
-    }
-
-    useEffect(() => {
-        getData();
-    }, []);
-
-    const handleUserClicked = (e, id) => {
-        e.preventDefault();
-        if (isSmallScreen) {
-            setID(id);
-            setVista("conversacion");
-        } else {
-            setUserID(id);
-            handleOpenSendMessage("xxl");
-        }
-    };
+    const {
+        mensajes,
+        handleUserClicked,
+        handleDeleteConversation,
+        clasesBotonesFormulario,
+    } = useMensajes({
+        handleOpenSendMessage,
+        isSmallScreen,
+        setUserID,
+    });
 
     return (
         <>
