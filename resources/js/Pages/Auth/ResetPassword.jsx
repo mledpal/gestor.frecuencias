d4d429a949ffec5c234a6cc96da19426cf527bff
Miguel Ledesma Palacios <mledpal@gmail.com>
2024-03-02 12:20:57 +0100

    Fix ResetPassword. Mostraba las dos vistas a la vez

--- resources/js/Pages/Auth/ResetPassword.jsx
+++ resources/js/Pages/Auth/ResetPassword.jsx
@@ -29,8 +29,7 @@ export default function ResetPassword({ token, email }) {
     return (
         <GuestLayout>
             <Head title="Reiniciar Contraseña" />
-            (isSmallScreen ? (<Walkie form="reset" />) : (<Radio form="reset" />
-            ))
+            {isSmallScreen ? <Walkie form="reset" /> : <Radio form="reset" />}
         </GuestLayout>
     );
 }
