6f8dbae2f1e93ce2c067c0429a643427fc3b183e
Miguel Ledesma Palacios <mledpal@gmail.com>
2024-03-03 18:20:43 +0100

    Fix para Contacto,

--- database/seeders/TablaTipoContactoSeeder.php
+++ database/seeders/TablaTipoContactoSeeder.php
@@ -13,13 +13,13 @@ class TablaTipoContactoSeeder extends Seeder
      */
     public function run(): void
     {
-        TipoContacto::firstOrCreate(['nombre' => 'Desconocido', 'color' => 'gray-900']);
-        TipoContacto::firstOrCreate(['nombre' => 'Aviación', 'color' => 'red-600']);
-        TipoContacto::firstOrCreate(['nombre' => 'Emisoras', 'color' => 'yellow-600']);
-        TipoContacto::firstOrCreate(['nombre' => 'Empresa', 'color' => 'pink-600']);
-        TipoContacto::firstOrCreate(['nombre' => 'Evento', 'color' => 'green-600']);
-        TipoContacto::firstOrCreate(['nombre' => 'Particular', 'color' => 'emerald-800']);
-        TipoContacto::firstOrCreate(['nombre' => 'Servicio', 'color' => 'blue-800']);
-        TipoContacto::firstOrCreate(['nombre' => 'URE', 'color' => 'sky-600']);
+        TipoContacto::create(['nombre' => 'Desconocido', 'color' => 'gray-900']);
+        TipoContacto::create(['nombre' => 'Aviación', 'color' => 'red-600']);
+        TipoContacto::create(['nombre' => 'Emisoras', 'color' => 'yellow-600']);
+        TipoContacto::create(['nombre' => 'Empresa', 'color' => 'pink-600']);
+        TipoContacto::create(['nombre' => 'Evento', 'color' => 'green-600']);
+        TipoContacto::create(['nombre' => 'Particular', 'color' => 'emerald-800']);
+        TipoContacto::create(['nombre' => 'Servicio', 'color' => 'blue-800']);
+        TipoContacto::create(['nombre' => 'URE', 'color' => 'sky-600']);
     }
 }
