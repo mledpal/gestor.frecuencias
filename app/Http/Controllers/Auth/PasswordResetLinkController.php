11b2f237046b0ea8b360d6c700dec42c941c4e50
Miguel Ledesma Palacios <mledpal@gmail.com>
2024-03-11 21:49:48 +0100

    FIX Cambia de vista al resetear la contrasñea

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -41,7 +41,7 @@ class PasswordResetLinkController extends Controller
         );
 
         if ($status == Password::RESET_LINK_SENT) {
-            return back()->with('status', __($status));
+            return redirect()->route('index')->with('status', __($status));
         }
 
         throw ValidationException::withMessages([
