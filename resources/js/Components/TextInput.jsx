bdd0a2fcb01e454c11feef1041d90ac8b9323537
Miguel Ledesma Palacios <116297993+mledpal@users.noreply.github.com>
2024-07-04 17:36:58 +0200

    Corrijo los problemas de colores cuando el tema es claro u oscuro en el TextInput

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -17,7 +17,7 @@ export default forwardRef(function TextInput(
             {...props}
             type={type}
             className={
-                "border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm text-white" +
+                "border-gray-500 text-white bg-[#121827] focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" +
                 className
             }
             ref={input}
